ae137f390dbbd5bdaab76727b8a14aa19e77f46d
Simon Cardona <simon.cdna@proton.me>
2025-08-16 01:14:09 +0200

    feat(main): read function

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("./messages.txt")
+	if err != nil {
+		fmt.Println("couln't open this file: messages.txt", err)
+	}
+	defer file.Close()
+
+	for {
+		buffer := make([]byte, 8)
+
+		n, err := file.Read(buffer)
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Println("read error:", err)
+			return
+		}
+
+		stringedBuffer := string(buffer[:n])
+		fmt.Printf("read: %s\n", stringedBuffer)
+	}
 }
