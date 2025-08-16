686ba8da0bfbb59937ed579da8ecd6300d9cb419
Simon Cardona <simon.cdna@proton.me>
2025-08-16 16:17:02 +0200

    refactor(main): move main to cmd/tcpListener

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -5,7 +5,43 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
+	"net"
 )
+
+const (
+	port = 42069
+)
+
+func main() {
+	tcpAddr := net.TCPAddr{
+		Port: port,
+	}
+
+	listener, err := net.ListenTCP("tcp", &tcpAddr)
+	if err != nil {
+		log.Fatal("error listening tcp", err)
+	}
+	defer listener.Close()
+
+	fmt.Println("Listening for TCP traffic on", port)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("connection error:", err)
+		}
+
+		fmt.Println("Accepted connection from", conn.RemoteAddr())
+		linesChannel := getLinesChannel(conn)
+
+		for line := range linesChannel {
+			fmt.Println(line)
+		}
+
+		fmt.Println("Connection to ", conn.RemoteAddr(), "closed")
+	}
+}
 
 func getLinesChannel(f io.ReadCloser) <-chan string {
 	ch := make(chan string)
