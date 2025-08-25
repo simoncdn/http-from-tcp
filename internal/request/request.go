74cf6a4a849211058de409ce2b70f9c254c987d9
Simon Cardona <simon.cdna@proton.me>
2025-08-25 22:48:34 +0200

    feat(internal/request): create requestFromReader function

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,4 +1,54 @@
 package request
 
-func Request() {
+import (
+	"errors"
+	"io"
+	"strings"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	requestLine, err := parseRequestLine(data)
+	if err != nil {
+		return nil, err
+	}
+
+	request := &Request{
+		*requestLine,
+	}
+
+	return request, nil
+}
+
+func parseRequestLine(data []byte) (*RequestLine, error) {
+	dataString := string(data)
+	parts := strings.Split(dataString, "\r\n")
+
+	requestLineParts := strings.Split(parts[0], " ")
+	if len(requestLineParts) != 3 {
+		return nil, errors.New("invalid request line")
+	}
+	httpVersion := strings.Split(requestLineParts[2], "/")[1]
+
+	requestLine := RequestLine{
+		HttpVersion:   httpVersion,
+		RequestTarget: requestLineParts[1],
+		Method:        requestLineParts[0],
+	}
+
+	return &requestLine, nil
 }
